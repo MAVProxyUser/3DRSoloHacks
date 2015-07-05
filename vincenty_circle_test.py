d6c1fff3454631486fedf86f9df3eea01ac5c8bf
Kevin Finisterre <kf@digitalmunition.com>
2015-07-05 19:29:27 -0400

    Add in Vincinty inverse code, and bearing code. Test the relocation of a circle to .5km away

--- vincenty_circle_test.py
+++ vincenty_circle_test.py
@@ -96,7 +96,7 @@ print "\n"
 print vinc_pt(40.12076, -83.07773, 10, 200)
 
 # inputs
-radius = 1000.0 # m - the following code is an approximation that stays reasonably accurate for distances < 100km
+radius = 15.0 # m - the following code is an approximation that stays reasonably accurate for distances < 100km
 centerLat = 40.12076
 # latitude of circle center, decimal degrees
 centerLon = -83.07773
@@ -107,6 +107,10 @@ N = 10 # number of discrete sample points to be generated along the circle
 
 # generate points
 circlePoints = []
+#point['lat']=centerLat
+#point['lon']=centerLon
+#circlePoints.append(point)
+
 for k in xrange(N):
     # compute
     angle = math.pi*2*k/N
@@ -119,6 +123,83 @@ for k in xrange(N):
     circlePoints.append(point)
 
 print "\n"
-print circlePoints
-print "\n"
+#print circlePoints
 
+for point in circlePoints:
+    print point
+
+def bearing(pointA, pointB):
+
+    # Calculates the bearing between two points.
+    #
+    # :Parameters:
+    #   - `pointA: The tuple representing the latitude/longitude for the
+    #     first point. Latitude and longitude must be in decimal degrees
+    #   - `pointB: The tuple representing the latitude/longitude for the
+    #     second point. Latitude and longitude must be in decimal degrees
+    #
+    # :Returns:
+    #   The bearing in degrees
+    #
+    # :Returns Type:
+    #   float
+
+    # if (type(pointA) != tuple) or (type(pointB) != tuple):
+    #     raise TypeError("Only tuples are supported as arguments")
+
+    lat1 = math.radians(pointA[0])
+    lat2 = math.radians(pointB[0])
+
+    diffLong = math.radians(pointB[1] - pointA[1])
+
+    x = math.sin(diffLong) * math.cos(lat2)
+    y = math.cos(lat1) * math.sin(lat2) - (math.sin(lat1)
+            * math.cos(lat2) * math.cos(diffLong))
+
+    initial_bearing = math.atan2(x, y)
+
+    # Now we have the initial bearing but math.atan2 return values
+    # from -180 to + 180 which is not what we want for a compass bearing
+    # The solution is to normalize the initial bearing as shown below
+    initial_bearing = math.degrees(initial_bearing)
+    compass_bearing = (initial_bearing + 360) % 360
+
+    return compass_bearing
+
+
+# Point #1 - 40.12076, -83.07773
+# https://www.google.com/maps/dir/40.12083920247139,++-83.07758744139487/40.12076,-83.0775538/40.120888152290696,+-83.07767554745823/40.120888152290696,++-83.07778445254178/40.12083920247139,+-83.07787255860514/40.12076,++-83.07790621212672/40.12068079752861,++-83.07787255860514/40.1206318477093,+-83.07778445254178/40.1206318477093,+-83.07767554745823/40.12076,+-83.07773/@40.1205266,-83.080423,17z/data=!3m1!4b1!4m53!4m52!1m5!1m1!1s0x0:0x0!2m2!1d-83.0775874!2d40.1208392!1m0!1m5!1m1!1s0x0:0x0!2m2!1d-83.0776755!2d40.1208882!1m5!1m1!1s0x0:0x0!2m2!1d-83.0777845!2d40.1208882!1m5!1m1!1s0x0:0x0!2m2!1d-83.0778726!2d40.1208392!1m5!1m1!1s0x0:0x0!2m2!1d-83.0779062!2d40.12076!1m5!1m1!1s0x0:0x0!2m2!1d-83.0778726!2d40.1206808!1m5!1m1!1s0x0:0x0!2m2!1d-83.0777845!2d40.1206318!1m3!2m2!1d-83.0776755!2d40.1206318!1m3!2m2!1d-83.07773!2d40.12076!3e2
+#
+#Point #2 - 40.120662, -83.078250
+
+print "Bearing "  
+b = bearing((40.12076, -83.07773), (40.120662, -83.078250))
+print b 
+
+from math import radians, cos, sin, asin, sqrt
+
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points 
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians 
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula 
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a)) 
+    r = 6371 # Radius of earth in kilometers. Use 3956 for miles
+    return c * r
+
+
+# Old center to new center
+h = haversine(40.12076, -83.07773, 40.120662, -83.078250)
+print h 
+
+for point in circlePoints:
+    print vinc_pt(point['lat'], point['lon'], h, b)
+
+# print vinc_pt(40.12076, -83.07773, distance, bearing)
