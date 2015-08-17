0e4192a63f97e6ac565c67f3f1865d768cafe900
root <root@ip-10-51-131-75.ec2.internal>
2015-08-17 14:33:48 +0000

    Change from sqlite to mysql. Make sure you create a blank database named 'pyrit' first

--- SoloRainbowTables/pyrit_SoloLink.sh
+++ SoloRainbowTables/pyrit_SoloLink.sh
@@ -1,7 +1,7 @@
-pyrit eval
-pyrit -i dict.txt import_passwords
-for each in `cat SSIDs.txt` ; do  pyrit -e $each create_essid; done
-pyrit eval
-pyrit batch
-pyrit verify
-pyrit -o SoloRainBowTable_Pyrit export_hashdb
+pyrit -u mysql://root:root@localhost/pyrit eval
+pyrit -u mysql://root:root@localhost/pyrit -i dict.txt import_passwords
+for each in `cat SSIDs.txt` ; do  pyrit -u mysql://root:root@localhost/pyrit -e $each create_essid; done
+pyrit -u mysql://root:root@localhost/pyrit eval
+pyrit -u mysql://root:root@localhost/pyrit batch
+pyrit -u mysql://root:root@localhost/pyrit verify
+pyrit -u mysql://root:root@localhost/pyrit -o SoloRainBowTable_Pyrit export_hashdb
