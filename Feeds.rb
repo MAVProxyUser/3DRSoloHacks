87b3c4f29156db415d45febbb1db72bc9d6a5514
Kevin Finisterre <kf@digitalmunition.com>
2015-12-08 10:18:55 -0500

    Add detail about hidden data feeds Add case statement for the newly used Alpha feed

--- Feeds.rb
+++ Feeds.rb
@@ -1,10 +1,14 @@
 require 'rubygems'
 require 'json'
 
+# There are *hidden* / unused feeds. ExternalBeta is currently NOT used. Alpha was recently *lit up*
+# $ curl -H "Authorization: Token bd02efe84c11b41d11c4644e9b04ad4673deb6df" https://firmwarehouse.3dr.com/channels/
+# {"count":4,"next":null,"previous":null,"results":[{"name":"Production","id":1},{"name":"Beta","id":2},{"name":"ExternalBeta","id":3},{"name":"Alpha","id":5}]}
+
 
 beta = %x[curl -H "Authorization: Token bd02efe84c11b41d11c4644e9b04ad4673deb6df" https://firmwarehouse.3dr.com/products/?channel=Beta 2>/dev/null]
 prod = %x[curl -H "Authorization: Token bd02efe84c11b41d11c4644e9b04ad4673deb6df" https://firmwarehouse.3dr.com/products/?channel=Production 2>/dev/null]
-
+alpha = %x[curl -H "Authorization: Token bd02efe84c11b41d11c4644e9b04ad4673deb6df" https://firmwarehouse.3dr.com/products/?channel=Alpha 2>/dev/null]
 def print_relnotes(info)
 	parsed = JSON.parse(info)
 
@@ -28,8 +32,11 @@ if ARGV[0] == "prod"
   print_relnotes(prod)
 elsif ARGV[0] == "beta"
   print_relnotes(beta)
-elsif ARGV[0] == "both"
+elsif ARGV[0] == "alpha"
+  print_relnotes(alpha)
+elsif ARGV[0] == "all"
   print_relnotes(beta)
   print_relnotes(prod)
+  print_relnotes(alpha)
 end
 
